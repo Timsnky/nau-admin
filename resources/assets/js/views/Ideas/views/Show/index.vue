cb45fa8f0c3feb7f0376703efe1e7794eda30baa
Marios Fakiolas <m.fakiolas@hotmail.gr>
2017-05-30 17:17:07 +0300

    add articles section in idea show page

--- resources/assets/js/views/Ideas/views/Show/index.vue
+++ resources/assets/js/views/Ideas/views/Show/index.vue
@@ -19,6 +19,17 @@
                 </div>
             </div>
         </form>
+
+        <div v-if="hasArticles">
+            <h2>Articles</h2>
+            <ul>
+                <li
+                    v-for="article in idea.articles"
+                    :key="article.id">
+                    {{ article.title }}
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -35,6 +46,12 @@
                 .get(`https://api-naut.livesystems.ch/ideas/${this.$route.params.id}`)
                 .then(response => this.idea = response.data)
                 .catch(err => console.log('Show some error message here'));
+        },
+
+        computed: {
+            hasArticles() {
+                return this.idea.hasOwnProperty('articles') && this.idea.articles.length > 0;
+            }
         }
     }
 </script>
