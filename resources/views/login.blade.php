0787c838c7873f07b2942bd47e32853be02c7180
Michele Della Neve <michele.dellaneve@livesystems.ch>
2017-06-13 11:28:12 +0200

    https for google fonts

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -15,7 +15,7 @@
     <meta content="width=device-width, initial-scale=1" name="viewport"/>
     <meta content="" name="author"/>
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
-    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet"
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet"
           type="text/css"/>
     <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
     <link href="{{asset('assets/global/plugins/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet"
