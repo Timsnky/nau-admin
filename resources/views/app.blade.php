f738b6bbff1c9877f6e4a787822598aa509f2a81
mmihael <mihael.mamula@gmail.com>
2017-06-28 16:45:04 +0200

    Article base form

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -31,6 +31,7 @@
     <link href="{{asset('assets/global/css/components.min.css')}}" rel="stylesheet" id="style_components"
     type="text/css" />
     <link href="{{asset('assets/global/css/plugins.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('assets/global/plugins/bootstrap-wysihtml5/bootstrap-wysihtml5.css')}}" rel="stylesheet" type="text/css" />
     <!-- END THEME GLOBAL STYLES -->
     <!-- BEGIN THEME LAYOUT STYLES -->
     <link href="{{asset('assets/layouts/layout/css/layout.min.css')}}" rel="stylesheet" type="text/css" />
@@ -85,6 +86,10 @@
     type="text/javascript"></script>
     <script src="{{asset('assets/global/plugins/bootstrap-select/js/bootstrap-select.min.js')}}"
     type="text/javascript"></script>
+    <script src="{{asset('assets/global/plugins/bootstrap-wysihtml5/wysihtml5-0.3.0.js')}}"
+        type="text/javascript"></script>
+    <script src="{{asset('assets/global/plugins/bootstrap-wysihtml5/bootstrap-wysihtml5.js')}}"
+        type="text/javascript"></script>
     <!-- END CORE PLUGINS -->
     <!-- BEGIN THEME GLOBAL SCRIPTS -->
     <script src="{{asset('assets/global/scripts/app.min.js')}}" type="text/javascript"></script>
