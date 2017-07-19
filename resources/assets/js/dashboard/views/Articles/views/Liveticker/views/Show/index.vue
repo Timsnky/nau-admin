f5f5c4f420eb3ea1dde261399c0bb7a62788252e
Michele Della Neve <michele.dellaneve@livesystems.ch>
2017-07-19 08:55:18 +0200

    liveticker create form

--- resources/assets/js/dashboard/views/Articles/views/Liveticker/views/Show/index.vue
+++ resources/assets/js/dashboard/views/Articles/views/Liveticker/views/Show/index.vue
@@ -1,93 +1,84 @@
 <template>
-    <div>
-        <page-title title="Articles" sub-title="Liveticker" />
-
-        <div class="timeline">
-
-            <!-- TIMELINE ITEM -->
-            <div class="timeline-item" v-for="liveticker in livetickers">
-                <div class="timeline-badge">
-                    <img class="timeline-badge-userpic" :src="liveticker.user.avatar"></div>
-                <div class="timeline-body">
-                    <div class="timeline-body-arrow"> </div>
-                    <div class="timeline-body-head">
-                        <div class="timeline-body-head-caption">
-                            <a href="javascript:;" class="timeline-body-title font-blue-madison">{{ liveticker.user.name }}</a>
-                            <span class="timeline-body-time font-grey-cascade">Geposted {{ humanize(liveticker.created_at) }}</span>
-                        </div>
-                        <div class="timeline-body-head-actions">
-                            <div class="btn-group">
-                                <button class="btn btn-circle green btn-sm dropdown-toggle" type="button" data-toggle="dropdown" data-hover="dropdown" data-close-others="true"> Aktionen
-                                    <i class="fa fa-angle-down"></i>
-                                </button>
-                                <ul class="dropdown-menu pull-right" role="menu">
-                                    <li>
-                                        <router-link :to="{name: 'articles.livetickers.edit', params: { article: $route.params.article, liveticker: liveticker.id }}"><i class="fa fa-edit"></i> Bearbeiten</router-link>
-                                    </li>
-                                    <li class="divider"> </li>
-                                    <li>
-                                        <a href="#" @click.prevent="remove(liveticker)"><i class="fa fa-trash"></i> Löschen</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
+    <div class="timeline">
+        <div class="timeline-item">
+            <div class="timeline-badge">
+                <img class="timeline-badge-userpic" :src="me.avatar"></div>
+            <div class="timeline-body">
+                <div class="timeline-body-arrow"> </div>
+                <div class="timeline-body-head">
+                    <div class="timeline-body-head-caption">
+                        <a href="javascript:;" class="timeline-body-title font-blue-madison">{{ me.name }}</a>
+                        <span class="timeline-body-time font-grey-cascade">Neues Update erstellen</span>
                     </div>
-                    <div class="timeline-body-content linebreaks">
-                        <span class="font-grey-cascade">{{ liveticker.content }}</span>
+                    <div class="timeline-body-head-actions">
+                        <div class="btn-group">
+
+                        </div>
                     </div>
                 </div>
-            </div>
-            <!-- END TIMELINE ITEM -->
+                <div class="timeline-body-content linebreaks">
+                    <span class="font-grey-cascade">
+                        <div class="form-group">
+                            <textarea v-model="content" class="form-control" rows="6"></textarea>
+                        </div>
 
+                        <button type="button" class="btn btn-primary" @click="create">Erstellen</button>
+                    </span>
+                </div>
+            </div>
         </div>
+
+        <timeline-item v-for="liveticker in livetickers" :key="liveticker.id" :liveticker="liveticker" @delete="removeLiveticker(liveticker)">
+            {{ liveticker.content }}
+        </timeline-item>
     </div>
 </template>
 <script>
+    import TimelineItem from './components/TimelineItem'
+
     export default {
         data() {
             return {
-                livetickers: []
+                livetickers: [],
+                me: {},
+                content: '',
             }
         },
 
-        created() {
+        components: {
+            'timeline-item': TimelineItem
+        },
+
+        methods: {
+            create() {
+                Api.http
+                    .post(`/articles/${this.$route.params.article}/livetickers`, {
+                        content: this.content,
+                    }).then(response => {
+                        this.livetickers.unshift(response.data);
+                        this.content = '';
+                        Vue.toast('Liveticker wurde erstellt', {
+                            className : ['nau_toast','et-info'],
+                        });
+                    });
+            },
+
+            removeLiveticker(liveticker) {
+                this.livetickers.splice(this.livetickers.indexOf(liveticker), 1);
+            },
+        },
+
+
+
+        mounted() {
+            Api.http
+                .get(`/me`)
+                .then(response => this.me = response.data);
+
             Api.http
                 .get(`/articles/${this.$route.params.article}/livetickers`)
                 .then(response => this.livetickers = response.data);
         },
-
-        methods: {
-            humanize: (date) => {
-                return moment(date, moment.ISO_8601).fromNow();
-            },
-
-            remove(liveticker)  {
-                swal({
-                    title: 'Sind sie sicher?',
-                    text: "Der Eintrag kann nicht wiederhergestellt werden!",
-                    type: 'warning',
-                    showCancelButton: true,
-                    cancelButtonText: 'Abbrechen',
-                    cancelButtonColor: '#d33',
-                    confirmButtonText: 'Ja, löschen!'
-                }).then(() => {
-                    Api.http
-                        .delete(`/livetickers/${liveticker.id}`)
-                        .then(response => {
-                            this.livetickers.splice(this.livetickers.indexOf(liveticker), 1);
-                            Vue.toast('Liveticker wurde gelöscht', {
-                                className : ['nau_toast','et-info'],
-                            });
-                        })
-                        .catch(err => {
-                            console.log(err)
-                            Vue.toast('Ein Fehler ist aufgetreten', {
-                                className : ['nau_toast','nau_warning'],
-                            });
-                        });
-                })
-            },
-        }
     }
 </script>
 
