add26218cdd6302dd67b39f6d56681ab77941375
Michele Della Neve <michele.dellaneve@livesystems.ch>
2017-07-24 09:30:03 +0200

    clean up

--- resources/assets/js/dashboard/components/SurveySelectModal.vue
+++ resources/assets/js/dashboard/components/SurveySelectModal.vue
@@ -1,43 +1,56 @@
 <template>
-    <div>
-        <div class="row image_selection_filters">
-            <div class="col-md-6">
-                <div v-if="surveys.length > 0 || searchTerm !== ''" class="input-icon">
-                    <i class="fa fa-search"></i>
-                    <input
-                        type="search"
-                        class="form-control"
-                        placeholder="Search"
-                        name="searchTerm"
-                        v-model.trim="searchTerm">
+    <div class="modal fade" id="surveySelectionModal" tabindex="-1" role="dialog" aria-labelledby="surveySelectionModalLabel">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title">Umfragen</h4>
+                </div>
+                <div class="modal-body">
+                    <div>
+
+                        <div class="row image_selection_filters">
+                            <div class="col-md-6">
+                                <div v-if="surveys.length > 0 || searchTerm !== ''" class="input-icon">
+                                    <i class="fa fa-search"></i>
+                                    <input
+                                    type="search"
+                                    class="form-control"
+                                    placeholder="Search"
+                                    name="searchTerm"
+                                    v-model.trim="searchTerm">
+                                </div>
+                            </div>
+                        </div>
+
+                        <h2 v-if="!isLoaded" class="text-center">Loading...</h2>
+
+                        <div v-else-if="surveys.length > 0">
+                            <div class="row image_selection_rows">
+                                <div v-for="survey in surveys" class="col-md-6">
+                                    <a @click.prevent="dispatchSelected(survey.id)" href="#"><h5>{{ survey.question }}</h5></a>
+                                    <ul>
+                                        <li v-for="answer in survey.answers">{{ answer.answer }}</li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="clearfix">
+                                <pagination
+                                class="pull-right"
+                                :currentPage="currentPage"
+                                :pagesCount="pagesCount"
+                                :itemsPerPage="itemsPerPage"
+                                @navigate="navigate"/>
+                            </div>
+                        </div>
+
+                        <h2 v-if="surveys.length === 0" class="text-center">Keine Umfragen gefunden</h2>
+
+                    </div>
                 </div>
             </div>
         </div>
-
-        <h2 v-if="!isLoaded" class="text-center">Loading...</h2>
-
-        <div v-else-if="surveys.length > 0">
-            <div class="row image_selection_rows">
-                <div v-for="survey in surveys" class="col-md-6">
-                    <a @click.prevent="dispatchSelected(survey.id)" href="#"><h5>{{ survey.question }}</h5></a>
-                    <ul>
-                        <li v-for="answer in survey.answers">{{ answer.answer }}</li>
-                    </ul>
-                </div>
-            </div>
-
-            <div class="clearfix">
-                <pagination
-                    class="pull-right"
-                    :currentPage="currentPage"
-                    :pagesCount="pagesCount"
-                    :itemsPerPage="itemsPerPage"
-                    @navigate="navigate"/>
-            </div>
-        </div>
-
-        <h2 v-if="surveys.length === 0" class="text-center">Keine Umfragen gefunden</h2>
-
     </div>
 </template>
 <script>
@@ -56,21 +69,21 @@
 
         mounted() {
             this.getPaginatedData(this.currentPage)
-                .then(response => {
-                    const { data, current_page, per_page, last_page } = response.data;
+            .then(response => {
+                const { data, current_page, per_page, last_page } = response.data;
 
-                    this.surveys = data;
-                    this.currentPage = current_page;
-                    this.itemsPerPage = per_page;
-                    this.pagesCount = last_page;
-                    this.isLoaded = true;
-                })
-                .catch(err => {
-                    Vue.toast('Fehler beim laden der Umfragen. Versuchen Sie es später nocheinmal', {
-                        className : ['nau_toast','nau_warning'],
-                    });
-                    this.isLoaded = true;
+                this.surveys = data;
+                this.currentPage = current_page;
+                this.itemsPerPage = per_page;
+                this.pagesCount = last_page;
+                this.isLoaded = true;
+            })
+            .catch(err => {
+                Vue.toast('Fehler beim laden der Umfragen. Versuchen Sie es später nocheinmal', {
+                    className : ['nau_toast','nau_warning'],
                 });
+                this.isLoaded = true;
+            });
         },
 
         watch: {
@@ -82,18 +95,18 @@
         methods: {
             navigate(page) {
                 this.getPaginatedData(page)
-                    .then(response => {
-                        const { data, current_page, last_page, from } = response.data;
+                .then(response => {
+                    const { data, current_page, last_page, from } = response.data;
 
-                        this.surveys = data;
-                        this.currentPage = current_page;
-                        this.pagesCount = last_page;
-                    })
-                    .catch(err => {
-                        Vue.toast('Error in retreiving the Videos. Please retry again', {
-                            className : ['nau_toast','nau_warning'],
-                        });
+                    this.surveys = data;
+                    this.currentPage = current_page;
+                    this.pagesCount = last_page;
+                })
+                .catch(err => {
+                    Vue.toast('Error in retreiving the Videos. Please retry again', {
+                        className : ['nau_toast','nau_warning'],
                     });
+                });
             },
 
             getPaginatedData(page) {
@@ -118,15 +131,3 @@
         }
     }
 </script>
-
-<style>
-    .image_selection_filters {
-        padding-top: 10px;
-        padding-bottom: 10px;
-    }
-
-    .image_selection_rows {
-        padding-top: 10px;
-        padding-bottom: 10px;
-    }
-</style>
--- resources/assets/js/dashboard/views/Surveys/views/List/index.vue
+++ resources/assets/js/dashboard/views/Surveys/views/List/index.vue
@@ -1,18 +1,6 @@
 <template>
     <div>
-        <div class="modal fade" id="surveySelectionModal" tabindex="-1" role="dialog" aria-labelledby="surveySelectionModalLabel">
-            <div class="modal-dialog modal-lg" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <h4 class="modal-title">Umfragen</h4>
-                    </div>
-                    <div class="modal-body">
-                        <survey-select-modal></survey-select-modal>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <survey-select-modal></survey-select-modal>
 
         <page-title title="Umfragen" sub="Liste" />
 
