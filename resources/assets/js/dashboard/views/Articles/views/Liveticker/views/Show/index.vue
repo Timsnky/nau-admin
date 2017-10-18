578c2847b49d797e66379c2160fa68b50c0ec621
Michele Della Neve <michele.dellaneve@livesystems.ch>
2017-10-18 08:22:09 +0200

    delete all liveticker elements button

--- resources/assets/js/dashboard/views/Articles/views/Liveticker/views/Show/index.vue
+++ resources/assets/js/dashboard/views/Articles/views/Liveticker/views/Show/index.vue
@@ -1,44 +1,49 @@
 <template>
-    <div class="timeline">
-        <div class="timeline-item">
-            <div class="timeline-badge">
-                <img class="timeline-badge-userpic" :src="me.avatar"></div>
-            <div class="timeline-body">
-                <div class="timeline-body-arrow"> </div>
-                <div class="timeline-body-head">
-                    <div class="timeline-body-head-caption">
-                        <a href="javascript:;" class="timeline-body-title font-blue-madison">{{ me.name }}</a>
-                        <span class="timeline-body-time font-grey-cascade">Neues Update erstellen</span>
-                    </div>
-                    <div class="timeline-body-head-actions">
-                        <div class="btn-group">
+    <div>
+        <div class="text-right">
+            <button class="btn btn-danger" @click="deleteAll()"><i class="fa fa-trash"></i> Alle Posts löschen</button>
+        </div>
+        <div class="timeline">
+            <div class="timeline-item">
+                <div class="timeline-badge">
+                    <img class="timeline-badge-userpic" :src="me.avatar"></div>
+                <div class="timeline-body">
+                    <div class="timeline-body-arrow"> </div>
+                    <div class="timeline-body-head">
+                        <div class="timeline-body-head-caption">
+                            <a href="javascript:;" class="timeline-body-title font-blue-madison">{{ me.name }}</a>
+                            <span class="timeline-body-time font-grey-cascade">Neues Update erstellen</span>
+                        </div>
+                        <div class="timeline-body-head-actions">
+                            <div class="btn-group">
 
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="timeline-body-content">
-                    <span class="font-grey-cascade">
-                        <div class="form-group">
-                            <multiselect v-model="type" :options="options" label="name" :show-labels="false" :searchable="false" placeholder="Typ auswählen" />
-                        </div>
+                    <div class="timeline-body-content">
+                        <span class="font-grey-cascade">
+                            <div class="form-group">
+                                <multiselect v-model="type" :options="options" label="name" :show-labels="false" :searchable="false" placeholder="Typ auswählen" />
+                            </div>
 
-                        <socialmedia-input v-if="type.value === 'socialmedia'" @new-post="fetchLivetickers" />
-                        <body-input v-if="type.value === 'body'" @new-post="fetchLivetickers" />
-                        <external-video-input v-if="type.value === 'external-video'" @new-post="fetchLivetickers" />
-                        <comment-input v-if="type.value === 'comment'" @new-post="fetchLivetickers" />
-                    </span>
+                            <socialmedia-input v-if="type.value === 'socialmedia'" @new-post="fetchLivetickers" />
+                            <body-input v-if="type.value === 'body'" @new-post="fetchLivetickers" />
+                            <external-video-input v-if="type.value === 'external-video'" @new-post="fetchLivetickers" />
+                            <comment-input v-if="type.value === 'comment'" @new-post="fetchLivetickers" />
+                        </span>
+                    </div>
                 </div>
             </div>
+
+            <timeline-item v-for="liveticker in livetickers" :key="liveticker.id" :liveticker="liveticker" @delete="removeLiveticker(liveticker)">
+
+                <socialmedia-element v-if="liveticker.type === 'socialmedia'" :element="liveticker" />
+                <external-video-element v-if="liveticker.type === 'externalvideo'" :element="liveticker" />
+                <body-element v-if="liveticker.type === 'body'">{{ liveticker.content }}</body-element>
+                <comment-element v-if="liveticker.type === 'comment'" :comment="liveticker" />
+
+            </timeline-item>
         </div>
-
-        <timeline-item v-for="liveticker in livetickers" :key="liveticker.id" :liveticker="liveticker" @delete="removeLiveticker(liveticker)">
-
-            <socialmedia-element v-if="liveticker.type === 'socialmedia'" :element="liveticker" />
-            <external-video-element v-if="liveticker.type === 'externalvideo'" :element="liveticker" />
-            <body-element v-if="liveticker.type === 'body'">{{ liveticker.content }}</body-element>
-            <comment-element v-if="liveticker.type === 'comment'" :comment="liveticker" />
-
-        </timeline-item>
     </div>
 </template>
 <script>
@@ -103,6 +108,22 @@
                 Api.http
                     .get(`/articles/${this.$route.params.article}/livetickers`)
                     .then(response => this.livetickers = response.data);
+            },
+
+            deleteAll() {
+                swal({
+                    title: 'Sind sie sicher?',
+                    text: "Die Einträge kann nicht wiederhergestellt werden!",
+                    type: 'warning',
+                    showCancelButton: true,
+                    cancelButtonText: 'Abbrechen',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Ja, alle löschen!'
+                }).then(() => {
+                    this.livetickers.forEach((liveticker) => {
+                        this.removeLiveticker(liveticker);
+                    });
+                });
             }
         },
 
