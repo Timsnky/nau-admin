10749dc1a4b42210af3f759c5f09f365a7c1c961
Michele Della Neve <michele.dellaneve@livesystems.ch>
2017-11-01 09:54:55 +0100

    topic moving

--- resources/assets/js/dashboard/views/TopicsArticles/views/mixins/Form.vue
+++ resources/assets/js/dashboard/views/TopicsArticles/views/mixins/Form.vue
@@ -1,90 +1,107 @@
 <template>
     <div>
-        <page-title :title="pageTitle"/>
+        <div class="row">
 
-        <form @submit.prevent="handleSubmit">
-            <div class="form-body">
-                <div class="form-group">
-                    <label for="title">Titel</label>
-                    <input
-                    id="title"
-                    type="text"
-                    name="title"
-                    v-model.trim="article.title"
-                    placeholder="Titel"
-                    class="form-control">
+            <div class="col-md-6">
+                <page-title :title="pageTitle"/>
+
+                <form @submit.prevent="handleSubmit">
+                    <div class="form-body">
+                        <div class="form-group">
+                            <label for="title">Titel</label>
+                            <input
+                            id="title"
+                            type="text"
+                            name="title"
+                            v-model.trim="article.title"
+                            placeholder="Titel"
+                            class="form-control">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label>Autors</label>
+                        <multiselect
+                        v-model="article.authors"
+                        :options="authors.list"
+                        placeholder="Type to search author"
+                        label="name"
+                        :max-height="500"
+                        :clear-on-select="false"
+                        :close-on-select="false"
+                        track-by="id"
+                        :multiple="true"
+                        open-direction="bottom"
+                        :internal-search="false"
+                        @search-change="searchAuthors"
+                        @remove="removeAuthor"
+                        @select="addAuthor"
+                    >
+                    </multiselect>
                 </div>
-            </div>
+
+                <div class="form-body">
+                    <div class="form-group">
+                        <label for="time">Uhrzeit</label>
+                        <input
+                        id="time"
+                        type="text"
+                        name="time"
+                        v-model.trim="time"
+                        placeholder="10:50"
+                        class="form-control">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label>Channel</label>
+                    <select class="form-control" v-model="article.channel.id">
+                        <option v-bind:value="channel.id" v-for="channel in channels">
+                            {{ channel.display_name }}
+                        </option>
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label class="mt-checkbox no_margin_bottom">
+                        <input type="checkbox" v-model="article.dooh.should_include_video" @change="resetArticleRegions()" value="true"> Sollte ein Dooh Video enthalten
+                        <span></span>
+                    </label>
+                </div>
+
+                <div class="form-actions">
+                    <button
+                        class="btn blue"
+                        type="submit"
+                        :disabled="!article.title">
+                        {{ submitLabel }}
+                    </button>
+                </div>
+            </form>
+
+        </div>
+        <div class="col-md-6">
+            <page-title title="Artikel verschieben"/>
+            <mover v-model="newTopic" :article-id="article.id" :current-topic-id="topicId"></mover>
 
             <div class="form-group">
-                <label>Autors</label>
-                <multiselect
-                v-model="article.authors"
-                :options="authors.list"
-                placeholder="Type to search author"
-                label="name"
-                :max-height="500"
-                :clear-on-select="false"
-                :close-on-select="false"
-                track-by="id"
-                :multiple="true"
-                open-direction="bottom"
-                :internal-search="false"
-                @search-change="searchAuthors"
-                @remove="removeAuthor"
-                @select="addAuthor"
-            >
-            </multiselect>
-        </div>
-
-        <div class="form-body">
-            <div class="form-group">
-                <label for="time">Uhrzeit</label>
-                <input
-                id="time"
-                type="text"
-                name="time"
-                v-model.trim="time"
-                placeholder="10:50"
-                class="form-control">
+                <button class="btn blue" @click="moveArticle" :disabled="!(newTopic && newTopic.id)">Verschieben</button>
             </div>
         </div>
-
-        <div class="form-group">
-            <label>Channel</label>
-            <select class="form-control" v-model="article.channel.id">
-                <option v-bind:value="channel.id" v-for="channel in channels">
-                    {{ channel.display_name }}
-                </option>
-            </select>
-        </div>
-
-        <div class="form-group">
-            <label class="mt-checkbox no_margin_bottom">
-                <input type="checkbox" v-model="article.dooh.should_include_video" @change="resetArticleRegions()" value="true"> Sollte ein Dooh Video enthalten
-                <span></span>
-            </label>
-        </div>
-
-        <div class="form-actions">
-            <button
-            class="btn btn-primary"
-            type="submit"
-            :disabled="!article.title">
-            {{ submitLabel }}
-        </button>
     </div>
-</form>
 </div>
 </template>
 
 <script>
+    import Mover from '../components/ArticleMover';
+
     export default {
         data() {
             return {
                 pageTitle: '',
                 submitLabel: '',
                 channels: [],
+                newTopic: null,
                 authors: {
                     list: [],
                     removed: [],
@@ -115,6 +132,12 @@
             }
         },
 
+        computed: {
+            topicId() {
+                return this.$route.params.topicID;
+            }
+        },
+
         created() {
             this.initializeChannels();
         },
@@ -137,6 +160,27 @@
                 this.authors.removed.push(author);
             },
 
+            moveArticle() {
+                if(!this.newTopic.id) {
+                    return false;
+                }
+
+                Promise.all([
+                    Api.http.delete(`/topics/${this.topicId}/articles/${this.article.id}`),
+                    Api.http.put(`/topics/${this.newTopic.id}/articles/${this.article.id}`),
+                ]).then(() => {
+                    Vue.toast('Artikel verschoben.', {
+                        className : ['nau_toast','nau_success'],
+                    });
+                    this.$router.push({name: 'resources.day', params: { date: this.date }})
+                }).catch((error) => {
+                    console.error(error);
+                    Vue.toast('Artikel konnte nicht verschoben werden.', {
+                        className : ['nau_toast','nau_warning'],
+                    });
+                })
+            },
+
             searchAuthors: _.debounce(function (query) {
                 if(!query) {
                     return false;
@@ -149,5 +193,9 @@
                     });
             }, 500),
         },
+
+        components: {
+            Mover,
+        }
     }
 </script>
